ce1ac5dca6c73eb20e9e1cb4bc7c367a40279fcd
Dirli <git@dirli.net>
2022-12-05 19:00:03 +0100

    Setup logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate slog;
+
+use slog::Drain;
+
+fn main() -> anyhow::Result<()> {
+    let log = slog::Logger::root(
+        slog_async::Async::default(slog_term::term_full().fuse()).fuse(),
+        o!(),
+    );
+
+    let _global_log_guard = slog_scope::set_global_logger(log.clone());
+    slog_stdlog::init()?;
+
+    info!(log, "Starting Scape");
+
+    Ok(())
 }
