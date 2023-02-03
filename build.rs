2b604b4931a50c2b673b114b578a651da5e80bc3
Dirli-V <github@dirli.net>
2023-02-03 18:08:32 +0100

    Clean up nix files

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let deps = system_deps::Config::new().probe().unwrap();
-    for p in deps.all_link_paths() {
-        println!("cargo:rustc-link-arg=-Wl,-rpath={}", p.display());
-    }
-}
--- flake.nix
+++ flake.nix
@@ -6,20 +6,22 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
-      devShell."x86_64-linux" = pkgs.mkShell {
-        # pkgs.mesa is needed because it provides libgdb (which could be used from other packages too)
-        nativeBuildInputs = [
-          pkgs.udev
-          pkgs.dbus
-          pkgs.wayland
-          pkgs.seatd
-          pkgs.pkg-config
-          pkgs.libxkbcommon
-          pkgs.mesa
-          pkgs.libinput
-          pkgs.egl-wayland
-          pkgs.libGL
-          pkgs.llvmPackages.bintools
+      devShell."x86_64-linux" = with pkgs; pkgs.mkShell {
+        buildInputs = [
+          udev
+          dbus
+          wayland
+          seatd
+          pkg-config
+          libxkbcommon
+          mesa
+          libinput
+          egl-wayland
+          libGL
+          llvmPackages.bintools
+        ];
+        LD_LIBRARY_PATH = with pkgs; lib.strings.makeLibraryPath [
+          libglvnd
         ];
       };
     };
