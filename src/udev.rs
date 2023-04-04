9005e84e98f83cfe74b69b133bb0f6c0f6bde108
Dirli-V <github@dirli.net>
2023-04-04 17:56:56 +0200

    Improve VT switch handling

--- src/udev.rs
+++ src/udev.rs
@@ -287,11 +287,13 @@ pub fn run_udev() {
         .insert_source(notifier, move |event, &mut (), data| match event {
             SessionEvent::PauseSession => {
                 libinput_context.suspend();
+                info!("pausing session");
                 for backend in data.state.backend_data.backends.values() {
                     backend.drm.pause();
                 }
             }
             SessionEvent::ActivateSession => {
+                info!("resuming session");
                 if let Err(err) = libinput_context.resume() {
                     tracing::error!("Failed to resume libinput context: {:?}", err);
                 }
@@ -299,14 +301,19 @@ pub fn run_udev() {
                     .state
                     .backend_data
                     .backends
-                    .iter()
+                    .iter_mut()
                     .map(|(handle, backend)| (*handle, backend))
                 {
                     backend.drm.activate();
-                    for surface in backend.surfaces.values() {
+                    for surface in backend.surfaces.values_mut() {
                         if let Err(err) = surface.compositor.surface().reset_state() {
                             warn!("Failed to reset drm surface state: {}", err);
                         }
+                        // reset the buffers after resume to trigger a full redraw
+                        // this is important after a vt switch as the primary plane
+                        // has no content and damage tracking may prevent a redraw
+                        // otherwise
+                        surface.compositor.reset_buffers();
                     }
                     handle.insert_idle(move |data| data.state.render(node, None));
                 }
@@ -740,12 +747,6 @@ fn scan_connectors(
                 }
             };
 
-            let size = mode.size();
-            let mode = Mode {
-                size: (size.0 as i32, size.1 as i32).into(),
-                refresh: mode.vrefresh() as i32 * 1000,
-            };
-
             let interface_short_name = match connector_info.interface() {
                 drm::control::connector::Interface::DVII => Cow::Borrowed("DVI-I"),
                 drm::control::connector::Interface::DVID => Cow::Borrowed("DVI-D"),
@@ -778,6 +779,7 @@ fn scan_connectors(
                 0,
             )
                 .into();
+            let mode = Mode::from(mode);
             output.change_current_state(Some(mode), None, None, Some(position));
             output.set_preferred(mode);
             space.map_output(&output, position);
@@ -1237,13 +1239,22 @@ impl AnvilState<UdevData> {
                 warn!("Error during rendering: {:?}", err);
                 match err {
                     SwapBuffersError::AlreadySwapped => true,
+                    // If the device has been deactivated do not reschedule, this will be done
+                    // by session resume
+                    SwapBuffersError::TemporaryFailure(err)
+                        if matches!(
+                            err.downcast_ref::<DrmError>(),
+                            Some(&DrmError::DeviceInactive)
+                        ) =>
+                    {
+                        false
+                    }
                     SwapBuffersError::TemporaryFailure(err) => matches!(
                         err.downcast_ref::<DrmError>(),
-                        Some(&DrmError::DeviceInactive)
-                            | Some(&DrmError::Access {
-                                source: drm::SystemError::PermissionDenied,
-                                ..
-                            })
+                        Some(&DrmError::Access {
+                            source: drm::SystemError::PermissionDenied,
+                            ..
+                        })
                     ),
                     SwapBuffersError::ContextLost(err) => {
                         panic!("Rendering loop lost: {}", err)
