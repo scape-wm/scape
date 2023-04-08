789f803b21b38cc0d9de846e8761371d6cb3a4bd
Dirli-V <github@dirli.net>
2023-04-08 22:04:48 +0200

    Use crane to build flake

--- flake.nix
+++ flake.nix
@@ -1,33 +1,85 @@
 {
-  description = "Flake for setting up pkg-config dependencies";
+  description = "Flake for scape";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
-    {
-      devShell."x86_64-linux" = with pkgs; pkgs.mkShell {
-        buildInputs = [
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nix-filter,
+    crane,
+    fenix,
+  }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"] (system: let
+      pkgs = import nixpkgs {inherit system;};
+      craneLib = crane.lib.${system}.overrideToolchain fenix.packages.${system}.stable.toolchain;
+
+      pkgDef = {
+        src = nix-filter.lib.filter {
+          root = ./.;
+          include = [
+            ./src
+            ./Cargo.toml
+            ./Cargo.lock
+            ./resources
+          ];
+        };
+        nativeBuildInputs = with pkgs; [pkg-config autoPatchelfHook];
+        buildInputs = with pkgs; [
           udev
           dbus
           wayland
           seatd
-          pkg-config
           libxkbcommon
-          mesa
           libinput
-          egl-wayland
-          libGL
+          mesa
           llvmPackages.bintools
         ];
-        LD_LIBRARY_PATH = with pkgs; lib.strings.makeLibraryPath [
+        runtimeDependencies = with pkgs; [
           libglvnd
-          /* needed for winit */
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
+          xorg.libX11 # needed for winit
+          xorg.libXcursor # needed for winit
+          xorg.libXrandr # needed for winit
+          xorg.libXi # needed for winit
         ];
       };
-    };
+
+      cargoArtifacts = craneLib.buildDepsOnly pkgDef;
+      scape = craneLib.buildPackage (pkgDef
+        // {
+          inherit cargoArtifacts;
+        });
+    in {
+      checks = {
+        inherit scape;
+      };
+
+      packages.default = scape;
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = scape;
+      };
+
+      devShells.default = pkgs.mkShell rec {
+        inputsFrom = builtins.attrValues self.checks.${system};
+        LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath (builtins.concatMap (d: d.runtimeDependencies) inputsFrom);
+      };
+    });
+
+  nixConfig = {
+    extra-trusted-substituters = ["https://nix-community.cachix.org"];
+    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+  };
 }
--- src/input_handler.rs
+++ src/input_handler.rs
@@ -946,7 +946,7 @@ fn process_keyboard_shortcut(modifiers: ModifiersState, keysym: Keysym) -> Optio
         ))
     } else if modifiers.logo && keysym == xkb::KEY_Return {
         // run terminal
-        Some(KeyAction::Run("alacritty".into()))
+        Some(KeyAction::Run("wezterm".into()))
     } else if modifiers.logo && (xkb::KEY_1..=xkb::KEY_9).contains(&keysym) {
         Some(KeyAction::Screen((keysym - xkb::KEY_1) as usize))
     } else if modifiers.logo && modifiers.shift && keysym == xkb::KEY_M {
