9bbc0d1697f26418a82b8f20967d73016ed3e154
Dirli-V <github@dirli.net>
2024-06-10 19:18:11 +0200

    Add nix cachix to trusted substituters

--- flake.nix
+++ flake.nix
@@ -124,7 +124,13 @@
     });
 
   nixConfig = {
-    extra-trusted-substituters = ["https://nix-community.cachix.org"];
-    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+    extra-trusted-substituters = [
+      "https://nix-community.cachix.org"
+      "https://scape.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "scape.cachix.org-1:DZrM365gcuH03W14BWTau3JjfbS+EomverT+ppifYDE="
+    ];
   };
 }
